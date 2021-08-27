50dcf3be36375832c2fc44e8c655ebf0c1f9fcd0
caroljunq <junqueiracarolina@live.com>
2021-08-27 15:48:21 -0300

    update code references

--- temp-gps-testing-code.ino
+++ temp-gps-testing-code.ino
@@ -1,17 +1,27 @@
+// Code References
+// https://randomnerdtutorials.com/guide-to-neo-6m-gps-module-with-arduino/
+// https://randomnerdtutorials.com/esp32-ds18b20-temperature-arduino-ide/
+// http://arduiniana.org/libraries/tinygpsplus/ 
+// https://github.com/ahmadlogs/arduino-ide-examples/tree/main/esp32-gps-tracker
 
+// Required libs
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <TinyGPS++.h>
 
+// TinyGPS object
 TinyGPSPlus gps;
 
+// Pin on ESp32
 static const int RXPin = 16, TXPin = 17;
+// Frequency transmission for GPS sensor
 static const uint32_t GPSBaud = 9600;
 
+// Using Serial Hardware to send message to ESP32
 HardwareSerial neogps(1);
 
 
-// GPIO where the DS18B20 is connected to
+// GPIO where the DS18B20 Temperature sensor is connected to
 const int oneWireBus = 4;     
 
 // Setup a oneWire instance to communicate with any OneWire devices
@@ -23,11 +33,13 @@ DallasTemperature sensors(&oneWire);
 
 void setup() {
 
-// Start the Serial Monitor
+  // Start the Serial Monitor
   Serial.begin(115200);
-  // Start the DS18B20 sensor
+  
+  // Start the Temperature sensor
   sensors.begin();
 
+  // Start GPS sensor
   neogps.begin(GPSBaud, SERIAL_8N1, RXPin, TXPin);
 
 
@@ -35,23 +47,34 @@ void setup() {
 
 void loop() {
 
+  // waiting data on gps is available
   while (neogps.available()){
       if (gps.encode(neogps.read())){
+  
+        // Information Collected
         float latitude = gps.location.lat();
         float longitude = gps.location.lng();
+        float altitd_meters = gps.altitude.meters();
+        float speed_km_h = gps.speed.kmph();
+        int date_year = gps.date.year();
+        int date_month = gps.date.month();
+        int date_day = gps.date.day();
+        int time_hour = gps.time.hour();
+        int time_minute = gps.time.minute();
+        int time_second = gps.time.second();
+        float temperatureC = sensors.getTempCByIndex(0);
+        float temperatureF = sensors.getTempFByIndex(0);
         Serial.print("Latitude="); 
         Serial.println(latitude,6);
         Serial.print("Longitude="); 
         Serial.println(longitude,6);
-        
-        sensors.requestTemperatures(); 
-        float temperatureC = sensors.getTempCByIndex(0);
-        float temperatureF = sensors.getTempFByIndex(0);
+        Serial.print("Altitude=");  
+        Serial.println(altitd_meters);
         Serial.print(temperatureC);
         Serial.println("ºC");
         Serial.print(temperatureF);
         Serial.println("ºF");
-          
+        Serial.println(" ");          
       }
    }
   
