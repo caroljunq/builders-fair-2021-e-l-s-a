de71d1cffa7b9a98231dcbe57569093962354868
Wu <wkimmy@8c859078b279.ant.amazon.com>
2021-09-01 10:20:03 -0300

    temperature graph component

--- elsap/src/app/app.module.ts
+++ elsap/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { TempComponent } from './temp/temp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    TempComponent
   ],
   imports: [
     BrowserModule
