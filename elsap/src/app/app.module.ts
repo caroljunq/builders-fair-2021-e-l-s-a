6bee767a1d5ff180bf57694ad2814ee30ade0fca
B da Silva <gubarbos@88e9fe78b2d6.ant.amazon.com>
2021-09-28 11:23:09 -0300

    adicionando as funcionalidades do mapa e marker no front-end

--- elsap/src/app/app.module.ts
+++ elsap/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { MarkerService } from './marker.service';
+
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { TempComponent } from './temp/temp.component';
@@ -15,8 +18,11 @@ import { NgApexchartsModule } from "ng-apexcharts";
   imports: [
     BrowserModule,
     NgApexchartsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    MarkerService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- elsap/src/app/map/map.component.ts
+++ elsap/src/app/map/map.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+import { MarkerService } from '../marker.service';
+
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
+
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements AfterViewInit {
+private map: any;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ -23.592013, -46.689212 ],
+      zoom: 15
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+  L.marker([-23.592013, -46.689212]).addTo(this.map)
+
+
+    tiles.addTo(this.map);
   }
 
+  constructor(private markerService: MarkerService) { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+    this.markerService.makeCapitalMarkers(this.map);
+  }
 }
