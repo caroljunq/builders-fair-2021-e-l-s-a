cf5063e7e67a65ca29f94ed4885456f7b27e1050
Wu <wkimmy@8c859078b279.ant.amazon.com>
2021-10-04 11:42:29 -0300

    added map http call to backend

--- elsap/src/app/map/map.component.ts
+++ elsap/src/app/map/map.component.ts
@@ -23,10 +23,34 @@ L.Marker.prototype.options.icon = iconDefault;
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
+var respLatlong = []
+function getLatLong() {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', "hhttps://4p9d4ru3bd.execute-api.us-east-1.amazonaws.com/prod/getLatLong");
+    xhr.addEventListener('loadend', () => {
+      if (xhr.status == 200) {
+        console.log("got the response")
+        resolve(JSON.parse(xhr.responseText)
+        );
+        const obj = JSON.parse(xhr.responseText)
+        //console.log(obj.body)
+        respLatlong = obj.body
+        console.log(respLatlong)
+        //return String(obj.body)
+      } else {
+        console.log(xhr.responseText);
+        reject("Não foi possível obter as negociações do servidor.");
+      }
+    });
+    xhr.send();
+  })
+}
+getLatLong();
+
 export class MapComponent implements AfterViewInit {
 private map: any;
-
-
   private initMap(): void {
     this.map = L.map('map', {
       center: [ -23.592013, -46.689212 ],
