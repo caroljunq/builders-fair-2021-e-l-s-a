5bfc09a4548a781491fa00298648c5516631c0bf
caroljunq <junqueiracarolina@live.com>
2021-10-07 11:51:52 -0300

    about routing

--- elsap/src/app/app-routing.module.ts
+++ elsap/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HistoricDashComponent } from './historic-dash/historic-dash.component';
 import { TempComponent } from "./temp/temp.component"
+import { AboutComponent } from './about/about.component';
+
 
 const routes: Routes = [
   { path: '', component:  TempComponent},
-  { path: 'historic', component:  HistoricDashComponent}
+  { path: 'historic', component:  HistoricDashComponent},
+  { path: 'about', component:  AboutComponent}
 ];
 
 @NgModule({
